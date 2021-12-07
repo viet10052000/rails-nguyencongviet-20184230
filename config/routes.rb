b4c206a7f0fe66570175b4040106abc1e678d648
vietnguyen <nguyencongvieth@gmail.com>
2021-12-07 02:00:27 +0000

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
+  root 'static_pages#home'
   get 'static_pages/home'
+  resources :books
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
