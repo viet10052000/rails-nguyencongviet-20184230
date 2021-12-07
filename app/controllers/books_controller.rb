63422d2b24a0c21a00bcee35c62183732249ba34
vietnguyen <nguyencongvieth@gmail.com>
2021-12-07 02:16:14 +0000

    Add image on book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,25 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
-
   # GET /books or /books.json
   def index
     @books = Book.all
   end
-
   # GET /books/1 or /books/1.json
   def show
   end
-
   # GET /books/new
   def new
     @book = Book.new
   end
-
   # GET /books/1/edit
   def edit
   end
-
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
 
+    @book.image.attach(params[:book][:image])
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: "Book was successfully created." }
@@ -34,7 +30,6 @@ class BooksController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
@@ -47,7 +42,6 @@ class BooksController < ApplicationController
       end
     end
   end
-
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
@@ -56,7 +50,6 @@ class BooksController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
@@ -66,5 +59,6 @@ class BooksController < ApplicationController
     # Only allow a list of trusted parameters through.
     def book_params
       params.require(:book).permit(:title, :number_of_pages)
+      params.require(:book).permit(:title, :number_of_pages, :image)
     end
 end
